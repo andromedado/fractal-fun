75121b3064d8179af2585a2390326983896c84f5
Shad Downey <shad.downey@gmail.com>
2020-02-21 20:18:43 -0500

    more shapes

--- js/app.js
+++ js/app.js
@@ -6,31 +6,28 @@ const ctx = c.getContext('2d');
 const width = c.offsetWidth;
 const height = c.offsetHeight;
 const padding = 10;
-
-/*
-const topmostPoint = new Point(width / 2, padding);
-let triangle = new EquilateralTriangle(topmostPoint, width - padding * 2);
-triangle.draw(ctx);
-triangle.fractify(ctx);
-/* */
-
-//shape = new Triangle([new Point(padding, padding), new Point(width - padding, height / 3), new Point(width / 3, height -padding)]);
-
-//*
-
 const length = 300;
+const shapes = [];
 
 let plaidSquare = new Square(new Point(padding, padding), length);
 plaidSquare.calculateProportion = function() {
     return 1 / Math.sqrt(this.sides + 1);
 };
+shapes.push(plaidSquare);
 
-plaidSquare.draw(ctx);
-plaidSquare.fractify(ctx);
+let altSquare = new Square(new Point(padding * 2 + length, padding), length);
+altSquare.calculateProportion = function () {
+    return 0.51;
+};
+shapes.push(altSquare);
 
 let triangle = new EquilateralTriangle(new Point(padding + length / 2, padding * 2 + length), length)
-triangle.draw(ctx);
-triangle.fractify(ctx);
+shapes.push(triangle);
+
+shapes.forEach((shape) => {
+    shape.draw(ctx);
+    shape.fractify(ctx);
+});
 
 /* */
 
